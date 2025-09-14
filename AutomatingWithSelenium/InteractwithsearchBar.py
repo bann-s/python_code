c9aa57bdac1a692b3472cbf57673f5f7a5db5984
bann-s <scariabinithaann@gmail.com>
2025-09-14 23:05:01 +0530

    modified AutomatingWithSelenium/InteractwithsearchBar.py

--- AutomatingWithSelenium/InteractwithsearchBar.py
+++ AutomatingWithSelenium/InteractwithsearchBar.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 
 driver = webdriver.Edge()
-driver.get("https://www.bing.com/")
+driver.get("https://www.python.org")
 search_bar = driver.find_element("name", "q") # Locate the search bar using its name attribute
 search_bar.clear()  # Clear any pre-filled text in the search bar
 search_bar.send_keys("getting started with python Selenium")    # Type the search query into the search bar
 search_bar.send_keys(Keys.RETURN)
 print(driver.title)  # Print the title of the current page
+print(driver.current_url)
